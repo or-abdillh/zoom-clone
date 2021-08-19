1996d49804d8c7617d5c0d5ea6594726f73bbf7c
or-abdillh <hans.abdillh05@gmail.com>
2021-08-19 20:47:30 +0800

    Create Header component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <Header></Header>
+</template>
+
+<script>
+   
+   import Header from './components/Header.vue';
+   
+   export default {
+      name: 'App',
+      components: {
+         Header
+      }
+   }
+   
+</script>
+
+<style lang="scss" >
+   
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+   
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+   <section class="header">
+      <p>Meet & chat</p>
+      <span>
+         <i class="far fa-edit"></i>
+      </span>
+   </section>
+</template>
+
+<script>
+   
+</script>
+
+<style lang="scss">
+   
+   @import 'src/style/_colors.scss';
+   
+   .header {
+      width: 100%;
+      padding: .6rem .5rem;
+      background: $arsenic;
+      font-size: 1.15rem;
+      font-family: 'Roboto', Sans-Serif;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      
+      p {
+         text-align: center;
+         display: block;
+         transform: translateX(130%);
+      }
+      
+      span {
+         background: $arsenic-sm;
+         padding: .3rem;
+         border-radius: 8px;
+         display: grid;
+         place-items: center;;
+         
+         i {
+            font-size: 1.35rem;;
+         }
+      }
+   }
+   
+</style>
